4031e81f60403f110cd32c7e0ab581f7cf697009
Sami Adam <sami.adam.usa@gmail.com>
2025-04-21 20:48:46 -0400

    Multivariate predict function implementation

--- classification/QuadraticDiscriminantAnalysis.py
+++ classification/QuadraticDiscriminantAnalysis.py
@@ -66,10 +66,11 @@ class MultivariateQDA(BaseEstimator, BaseModel, TransformerMixin):
         
         n_samples, n_features = X.shape
         self.classes = np.unique(y)
-        n_classes = len(self.classes)
+        self.n_classes_ = len(self.classes)
+        self.class_to_index_ = {label: idx for idx, label in enumerate(self.classes)}
         
-        means = np.zeros((n_classes, n_features))
-        covs = np.zeros((n_classes, n_features, n_features))
+        means = np.zeros((self.n_classes_, n_features))
+        covs = np.zeros((self.n_classes_, n_features, n_features))
         
         for i, k in enumerate(self.classes):
             X_k = X[y == k]
@@ -89,6 +90,29 @@ class MultivariateQDA(BaseEstimator, BaseModel, TransformerMixin):
         priors = class_counts / X.shape[0]
         self.priors_ = priors
 
+    def predict(self, X):
+        self._check_is_fitted()
+        
+        X = np.asarray(X)
+        
+        if X.ndim != 2:
+            raise ValueError("X must be a 2D array")
+        
+        probs = np.zeros((len(X), self.n_classes_))
+        for i, k in enumerate(self.classes):
+            cov_k = self.covariances_[i]
+            mean_k = self.class_means_[i]
+            
+            t1 = -0.5 * np.log(np.linalg.det(cov_k))
+            
+            diff = X - mean_k
+            inv_cov_k = np.linalg.inv(cov_k)
+            t2 = -0.5 * np.sum((diff @ inv_cov_k) * diff, axis=1)
+            
+            probs[:, i] = t1 + t2 + np.log(self.priors_[i])
+            
+        return self.classes[np.argmax(probs, axis= 1)]
+        
 if __name__ == "__main__":
     qda = UnivariateQDA()
     X = np.array([1, 2, 3, 4, 5, 6])
