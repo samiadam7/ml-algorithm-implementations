edf09b668b5095ceed8fae7fdfb77f63ffd350fd
Sami Adam <sami.adam.usa@gmail.com>
2025-04-22 16:13:53 -0400

    Enhance PrincipalComponentAnalysis class with improved input validation and variance ratio handling
    Updated the PrincipalComponentAnalysis class to include checks for 2-Dimensional input data and ensure that the number of components does not exceed the number of features. Added functionality to handle variance ratio thresholds for component selection, along with comprehensive test cases for various scenarios, including edge cases. The fit method now returns the instance for method chaining.

--- unsupervised/PrincipalComponentAnalysis.py
+++ unsupervised/PrincipalComponentAnalysis.py
@@ -1,20 +1,31 @@
 import numpy as np
-from base.base import BaseEstimator, TransformerMixin
+from base.base import BaseEstimator, TransformerMixin, BaseModel
+import warnings
 
-class PrincipalComponentAnalysis(BaseEstimator, TransformerMixin):
+class PrincipalComponentAnalysis(BaseEstimator, BaseModel, TransformerMixin):
     def __init__(self, n_components):
         
         if not isinstance(n_components, (int, float)):
             raise ValueError("n_components must be integer or floating point")
         
-        self.n_components = n_components
+        if isinstance(n_components, int):
+            self.n_components = n_components
+        else:
+            if 0 <= n_components <= 1:
+                self.n_components = n_components
         super().__init__()
         
     def fit(self, X):
         self._is_fitted = True
         
         X = np.asarray(X)
+        
+        if X.ndim != 2:
+            raise ValueError("X must be 2-Dimensional")
         self.n_samples_, self.n_features_ = X.shape
+        
+        if self.n_features_ < self.n_components:
+            raise ValueError(f"n_components must be less than the number of features ({self.n_features_})")
         
         self.mean_ = X.mean(axis= 0)
         X_centered = X - self.mean_
@@ -25,14 +36,34 @@ class PrincipalComponentAnalysis(BaseEstimator, TransformerMixin):
         e_vals = e_vals[idx]
         e_vecs = e_vecs[:, idx]
         
-        final_e_vals = e_vals[:self.n_components]
-        final_e_vecs = e_vecs[:, :self.n_components]
+        denom = e_vals.sum()
+        ratios = e_vals / denom
         
+        self.explained_variance_ratio_ = ratios
+        
+        if isinstance(self.n_components, int):
+            final_e_vals = e_vals[:self.n_components]
+            final_e_vecs = e_vecs[:, :self.n_components]
+            
+        else:
+            total_var = np.cumsum(self.explained_variance_ratio_)
+            idx_arr = np.argwhere(total_var >= self.n_components)
+            
+            if len(idx_arr) == 0:
+                warnings.warn(f"No number of components achieves {self.n_components:.2f} variance. Using all components.")
+                final_e_vals = e_vals
+                final_e_vecs = e_vecs
+            
+            else:
+                idx = idx_arr[0][0]
+                print(idx)
+                final_e_vals = e_vals[:idx+1]
+                final_e_vecs = e_vecs[:, :idx+1]
+                
         self._e_vals = final_e_vals
         self._e_vecs = final_e_vecs
         
-        
-        
+        return self
         
     def transform(self, X):
         self._check_is_fitted()
